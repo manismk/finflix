3bd4a88e038ed6e5968498847dd8e38ab0eb57dc
manismk <manismkmech@gmail.com>
2022-04-14 07:49:43 +0530

    fix: container-100

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 function App() {
   return (
     <div className="App  dark--theme">
-      <header className="App-header">
-        <h1 className="brand-title">
-          Welcome to <span>mockBee!</span>
-        </h1>
-      </header>
+      <div className="container--100"></div>
     </div>
   );
 }

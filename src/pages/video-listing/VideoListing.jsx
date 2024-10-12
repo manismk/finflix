256b5fbe9c465103a0f2361ffd6eb6131482cb98
Manikandan S <manismkmech@gmail.com>
2024-10-12 09:10:58 +0530

    fix: home loadiing

--- src/pages/video-listing/VideoListing.jsx
+++ src/pages/video-listing/VideoListing.jsx
@@ -9,16 +9,20 @@ export const VideoListing = () => {
     <>
       <h1 className="text--center m-v-2">{`Videos (${videoState.videos.length})`}</h1>
       <Filters />
-      {videoState.videos.length > 0 ? (
-        <div className="grid grid--3--cols">
-          {videoState.videos.map((videoDetails) => (
-            <VideoCard videoDetails={videoDetails} key={videoDetails._id} />
-          ))}
-        </div>
-      ) : (
-        <p className="text--center m-v-2 para--md ">{`No videos found for term "${
-          videoState.searchText
-        }" with "${videoState.category.split("-").join(" ")}" category`}</p>
+      {!videoState.isLoading && (
+        <>
+          {videoState.videos.length > 0 ? (
+            <div className="grid grid--3--cols">
+              {videoState.videos.map((videoDetails) => (
+                <VideoCard videoDetails={videoDetails} key={videoDetails._id} />
+              ))}
+            </div>
+          ) : (
+            <p className="text--center m-v-2 para--md ">{`No videos found for term "${
+              videoState.searchText
+            }" with "${videoState.category.split("-").join(" ")}" category`}</p>
+          )}
+        </>
       )}
       {videoState.isLoading && <Loader />}
       {videoState.showPlaylistModal && <PlaylistModal />}
